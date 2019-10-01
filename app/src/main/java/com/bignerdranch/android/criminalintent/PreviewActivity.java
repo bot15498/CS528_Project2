e418c03c9ca03737ce8513789c1f62cc20f13ec4
Theodore Konstantopoulos <konstantopoulostheodore@gmail.com>
2019-10-01 18:30:09 -0400

    Face detection attempt #2

--- app/src/main/java/com/bignerdranch/android/criminalintent/PreviewActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/PreviewActivity.java
@@ -1,16 +1,20 @@
 package com.bignerdranch.android.criminalintent;
 
-import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.util.SparseArray;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-import com.google.android.gms.vision.MultiProcessor;
-import com.google.android.gms.vision.Tracker;
+import com.bignerdranch.android.criminalintent.face.patch.SafeFaceDetector;
+import com.bignerdranch.android.criminalintent.face.photo.FaceView;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.face.Face;
 import com.google.android.gms.vision.face.FaceDetector;
 
@@ -19,15 +23,15 @@ import java.io.File;
 public class PreviewActivity extends AppCompatActivity {
 
     private ImageView imgDisplay;
-    private GraphicOverlay mGraphicOverlay;
+    private FaceDetector detector;
+    private Bitmap editedBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preview);
 
-        imgDisplay = (ImageView) findViewById(R.id.imgDisplay);
-        mGraphicOverlay = (GraphicOverlay) findViewById(R.id.faceOverlay);
+//        imgDisplay = (ImageView) findViewById(R.id.imgDisplay);
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
@@ -36,102 +40,48 @@ public class PreviewActivity extends AppCompatActivity {
                 Log.d(getClass().getName(), photoFilePath);
                 File imgFile = new File(photoFilePath);
                 Bitmap imgBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
-                imgDisplay.setImageBitmap(imgBitmap);
+//                imgDisplay.setImageBitmap(imgBitmap);
+
+                detector = new FaceDetector.Builder(getApplicationContext())
+                        .setTrackingEnabled(false)
+                        .setLandmarkType(FaceDetector.ALL_LANDMARKS)
+                        .build();
+
+                Detector<Face> safeDetector = new SafeFaceDetector(detector);
+
+                // Create a frame from the bitmap and run face detection on the frame.
+                Frame frame = new Frame.Builder().setBitmap(imgBitmap).build();
+                SparseArray<Face> faces = safeDetector.detect(frame);
+
+                if (faces.size() == 0) {
+                    Toast.makeText(getApplicationContext(), "No faces detected", Toast.LENGTH_LONG).show();
+                }
+
+                if (!safeDetector.isOperational()) {
+                    Log.w(getClass().getName(), "Face detector dependencies are not yet available.");
+
+                    // Check for low storage.  If there is low storage, the native library will not be
+                    // downloaded, so detection will not become operational.
+                    IntentFilter lowstorageFilter = new IntentFilter(Intent.ACTION_DEVICE_STORAGE_LOW);
+                    boolean hasLowStorage = registerReceiver(null, lowstorageFilter) != null;
+
+                    if (hasLowStorage) {
+                        Toast.makeText(this, "Low storage", Toast.LENGTH_LONG).show();
+                    }
+                }
+
+                FaceView overlay = (FaceView) findViewById(R.id.faceView);
+                overlay.setContent(imgBitmap, faces);
+
+                safeDetector.release();
             }
         }
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-
-        Context context = getApplicationContext();
-        FaceDetector detector = new FaceDetector.Builder(context)
-                .setClassificationType(FaceDetector.ALL_CLASSIFICATIONS)
-                .build();
-
-        detector.setProcessor(
-                new MultiProcessor.Builder<>(new GraphicFaceTrackerFactory())
-                        .build());
-
-        if (!detector.isOperational()) {
-            // Note: The first time that an app using face API is installed on a device, GMS will
-            // download a native library to the device in order to do detection.  Usually this
-            // completes before the app is run for the first time.  But if that download has not yet
-            // completed, then the above call will not detect any faces.
-            //
-            // isOperational() can be used to check if the required native library is currently
-            // available.  The detector will automatically become operational once the library
-            // download completes on device.
-            Log.w(getClass().getName(), "Face detector dependencies are not yet available.");
-        } else {
-//            detector.detect();
-        }
-    }
-
-    //==============================================================================================
-    // Graphic Face Tracker
-    //==============================================================================================
-
-    /**
-     * Factory for creating a face tracker to be associated with a new face.  The multiprocessor
-     * uses this factory to create face trackers as needed -- one for each individual.
-     */
-    private class GraphicFaceTrackerFactory implements MultiProcessor.Factory<Face> {
-        @Override
-        public Tracker<Face> create(Face face) {
-            return new GraphicFaceTracker(mGraphicOverlay);
-        }
-    }
-
-    /**
-     * Face tracker for each detected individual. This maintains a face graphic within the app's
-     * associated face overlay.
-     */
-    private class GraphicFaceTracker extends Tracker<Face> {
-        private GraphicOverlay mOverlay;
-        private FaceGraphic mFaceGraphic;
-
-        GraphicFaceTracker(GraphicOverlay overlay) {
-            mOverlay = overlay;
-            mFaceGraphic = new FaceGraphic(overlay);
-        }
-
-        /**
-         * Start tracking the detected face instance within the face overlay.
-         */
-        @Override
-        public void onNewItem(int faceId, Face item) {
-            mFaceGraphic.setId(faceId);
-        }
-
-        /**
-         * Update the position/characteristics of the face within the overlay.
-         */
-        @Override
-        public void onUpdate(FaceDetector.Detections<Face> detectionResults, Face face) {
-            mOverlay.add(mFaceGraphic);
-            mFaceGraphic.updateFace(face);
-        }
-
-        /**
-         * Hide the graphic when the corresponding face was not detected.  This can happen for
-         * intermediate frames temporarily (e.g., if the face was momentarily blocked from
-         * view).
-         */
-        @Override
-        public void onMissing(FaceDetector.Detections<Face> detectionResults) {
-            mOverlay.remove(mFaceGraphic);
-        }
-
-        /**
-         * Called when the face is assumed to be gone for good. Remove the graphic annotation from
-         * the overlay.
-         */
-        @Override
-        public void onDone() {
-            mOverlay.remove(mFaceGraphic);
-        }
+    protected void onDestroy() {
+        super.onDestroy();
+        detector.release();
     }
 
 }
