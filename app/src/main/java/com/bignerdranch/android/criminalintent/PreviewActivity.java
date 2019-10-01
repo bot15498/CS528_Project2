5c35fa8d9225c03e2ceb7932d4413f9eea4160ee
manasmehta18 <manasmehta.18@gmail.com>
2019-10-01 16:28:23 -0400

    image preview

--- app/src/main/java/com/bignerdranch/android/criminalintent/PreviewActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/PreviewActivity.java
@@ -1,19 +1,32 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+
+import java.io.File;
 
 public class PreviewActivity extends AppCompatActivity {
+
+    ImageView preview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preview);
-
+        preview = (ImageView) findViewById(R.id.imageView);
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
-            Log.d(getClass().getName(), extras.getString("photoFilePath"));
+            String imgFilePath = extras.getString("photoFilePath");
+            if(imgFilePath != null) {
+                Log.d(getClass().getName(), extras.getString("photoFilePath"));
+                File imgFile = new File(imgFilePath);
+                Bitmap imgBitMap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+                preview.setImageBitmap(imgBitMap);
+            }
         }
     }
 
